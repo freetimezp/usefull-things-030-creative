429c42e939df896021b2bdb7b7e46376381e0417
freetimezp <kraftukrnet@ukr.net>
2025-02-21 23:22:18 +0200

    03. usefull-things-030-creative. GSAP, Anime animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,90 @@ var followMouse = function followMouse() {
     ball.style.left = x + 'px';
     ball.style.top = y + 'px';
 };
+
+
+var textWrapper = document.querySelector(".intro-title");
+textWrapper.innerHTML = textWrapper.textContent.replace(/[\p{L}]/gu, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false }).add({
+    targets: '.intro-title .letter',
+    translateY: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: function (el, i) {
+        return 300 + 30 * i;
+    }
+}).add({
+    targets: '.intro-title .letter',
+    translateY: [0, -40],
+    translateZ: 0,
+    opacity: [1, 0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: function (el, i) {
+        return 100 + 30 * i;
+    }
+});
+
+
+var heroWrapper = document.querySelector(".hero-title");
+heroWrapper.innerHTML = heroWrapper.textContent.replace(/[\p{L}]/gu, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false }).add({
+    targets: '.hero-title .letter',
+    translateX: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: function (el, i) {
+        return 8000 + 30 * i;
+    }
+});
+
+
+
+TweenMax.to(".loading-screen", 2.2, {
+    delay: 3.8,
+    top: "-100%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 2, {
+    delay: 5.2,
+    y: 20,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".year", 2, {
+    delay: 5.3,
+    y: 20,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".bar", 2, {
+    delay: 7.2,
+    width: "0%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".project", 2, {
+    delay: 5.9,
+    y: 20,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".media ul li", 2, {
+    delay: 6.3,
+    y: 20,
+    opacity: 0,
+    ease: Power3.easeInOut
+}, 0.2);
+
+
+
